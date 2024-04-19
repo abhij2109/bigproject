c733bae1afc3bad1265c6595f3b7977f595021ba
abhij2109 <abhij2116@gmail.com>
2024-04-20 01:54:31 +0530

    Added the constants file for storing constants.

---
+++ src/main/java/com/abhi/bigproject/constants/ApplicationConstants.java
@@ -0,0 +1,9 @@
+package com.abhi.bigproject.constants;
+
+public class ApplicationConstants {
+
+    public ApplicationConstants(){
+    }
+    public static final String MYSQL_PROFILE = "mysql";
+
+}
